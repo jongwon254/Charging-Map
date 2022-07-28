0ebcccc5fb4a687b95d3ca37aa5a46fb4213625d
Joey Lee <jongwon254@gmail.com>
2022-07-29 01:56:24 +0200

    app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import * as Mapboxgl from 'mapbox-gl';
-import { environment } from 'src/environments/environment';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
 
-  ngOnInit(): void {
-
-    (Mapboxgl as any).accessToken = environment.map_token;
-    var map = new Mapboxgl.Map({
-      container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [9.183333, 48.783333], 
-      zoom: 9 
-    });
-  }
-  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { MapBoxComponent } from './map-box/map-box.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MapBoxComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
