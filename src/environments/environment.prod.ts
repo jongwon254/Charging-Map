c9e36310a9e22548abe793465acf3e901a3cfa44
Joey Lee <jongwon254@gmail.com>
2022-07-28 16:51:48 +0200

    mapbox token

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  mapbox_token: 'pk.eyJ1Ijoiam9uZ3dvbjI1NCIsImEiOiJjbDY1NHU2djYwMmo2M2NxZzZ1eDlzN2J5In0.0zMLYprMNLjpx8aOek0MbQ'
 };
