1ddc0129135b10508351387eed78b87291121e57
Joey Lee <jongwon254@gmail.com>
2022-07-28 20:52:15 +0200

    mapbox integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as Mapboxgl from 'mapbox-gl';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Charging-Map';
+export class AppComponent implements OnInit {
+
+  ngOnInit(): void {
+
+    (Mapboxgl as any).accessToken = environment.map_token;
+    var map = new Mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [9.183333, 48.783333], 
+      zoom: 9 
+    });
+  }
+  
 }
