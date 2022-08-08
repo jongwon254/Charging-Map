770761ae8eedea4a9bf7436e94cc7457fa393548
Joey Lee <jongwon254@gmail.com>
2022-08-08 02:27:57 +0200

    mapbox adding charging points

--- src/app/map-box/map-box.component.ts
+++ src/app/map-box/map-box.component.ts
@@ -31,63 +31,16 @@ export class MapBoxComponent implements OnInit {
     function setupMap(center: [number, number]) {
       const map = new Mapboxgl.Map({
         container: 'map',
-        style: 'mapbox://styles/mapbox/streets-v11',
+        style: 'mapbox://styles/jongwon254/cl6jsco53001x14ql93eabfxv',
         center: center,
-        zoom: 12 
+        zoom: 11 
       });
 
       map.addControl(new Mapboxgl.NavigationControl())
 
       map.addControl(new Mapboxgl.FullscreenControl())
 
-      map.on('load', () => {
-
-        map.addSource('markers', {
-          type: 'geojson',
-          data: {
-            type: 'FeatureCollection',
-            features: [
-              {
-                'type': 'Feature',
-                'geometry': {
-                  'type': 'Point',
-                  'coordinates': [9.183333, 48.783333]
-                },
-                'properties': {
-                  'title': 'Charging Point'
-                }
-              },
-              {
-                'type': 'Feature',
-                'geometry': {
-                  'type': 'Point',
-                  'coordinates': [7.77161, 49.443]
-                },
-                'properties': {
-                  'title': 'Charging Point'
-                }
-              }
-            ]
-          }
-        })
-  
-        map.addLayer({
-          'id': 'points',
-          'type': 'symbol',
-          'source': 'markers',
-          'layout': {
-            'icon-image':'car-15',
-            'text-field': ['get', 'title'],
-            'text-font': [
-            'Open Sans Semibold',
-            'Arial Unicode MS Bold'
-            ],
-            'text-offset': [0, 0.5],
-            'text-anchor': 'top'
-          }
-        })
-
-      })
+      
     }
   }
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  mapbox_token: 'pk.eyJ1Ijoiam9uZ3dvbjI1NCIsImEiOiJjbDY1NHU2djYwMmo2M2NxZzZ1eDlzN2J5In0.0zMLYprMNLjpx8aOek0MbQ'
+  mapbox_token: 'pk.eyJ1Ijoiam9uZ3dvbjI1NCIsImEiOiJjbDY1NG90NXUwMng1M2JubzQyaG0xNGxkIn0.KJ8M1xCIRpV8XjyVG-L8Pw'
+  //mapbox_token: 'pk.eyJ1Ijoiam9uZ3dvbjI1NCIsImEiOiJjbDY1NHU2djYwMmo2M2NxZzZ1eDlzN2J5In0.0zMLYprMNLjpx8aOek0MbQ'
+  
 };
